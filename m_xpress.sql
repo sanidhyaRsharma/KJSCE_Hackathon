485f20d795262f1ad14eec82e19e30a2896eba84
Ameya Daddikar <ameyadaddikar@gmail.com>
2018-10-05 18:53:49 +0530

    Added region names and postal codes to database

--- m_xpress.sql
+++ m_xpress.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.5.5-10.3.9-MariaDB)
 # Database: m_xpress
-# Generation Time: 2018-10-05 12:58:03 +0000
+# Generation Time: 2018-10-05 13:22:48 +0000
 # ************************************************************
 
 
@@ -59,15 +59,6 @@ CREATE TABLE `card` (
   CONSTRAINT `Fk_card_ward` FOREIGN KEY (`ward`) REFERENCES `ward` (`ward_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-LOCK TABLES `card` WRITE;
-/*!40000 ALTER TABLE `card` DISABLE KEYS */;
-
-INSERT INTO `card` (`card_id`, `timestamp`, `title`, `lat`, `lng`, `description`, `image`, `category`, `ward`, `user_id`)
-VALUES
-	(1,'2018-10-05 16:45:56','h1',0,0,'asd','fff','CRIME','A',0);
-
-/*!40000 ALTER TABLE `card` ENABLE KEYS */;
-UNLOCK TABLES;
 
 
 # Dump of table comment
@@ -146,7 +137,7 @@ LOCK TABLES `user` WRITE;
 
 INSERT INTO `user` (`user_id`, `username`, `email_id`, `oauth_provider`, `oauth_id`, `default_ward`)
 VALUES
-	(1,'sagar','anon@google.com','GOOGLE',0,'A');
+	(1,'sagar','anon@google.com','GOOGLE',0,'Ward B');
 
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -160,39 +151,40 @@ DROP TABLE IF EXISTS `ward`;
 CREATE TABLE `ward` (
   `ward_name` varchar(30) NOT NULL DEFAULT '',
   `office_address` varchar(200) DEFAULT '',
+  `ward_region` varchar(60) DEFAULT NULL,
+  `post_code` int(6) DEFAULT NULL,
   PRIMARY KEY (`ward_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 LOCK TABLES `ward` WRITE;
 /*!40000 ALTER TABLE `ward` DISABLE KEYS */;
 
-INSERT INTO `ward` (`ward_name`, `office_address`)
+INSERT INTO `ward` (`ward_name`, `office_address`, `ward_region`, `post_code`)
 VALUES
-	('',''),
-	('A',NULL),
-	('Ward B','‘B’ Ward Office Building, 121, Ramchandra Bhatt Marg , Opp.J.J.Hospital , Mumbai-400 009.'),
-	('Ward C','‘C’ ward Office Building, 76, Shrikant Palekar Marg, Sonapur Street, Chira Bazar-Kalbadevi, Mumbai – 400002, Near Marine Lines Railway Station, Chandanwadi.'),
-	('Ward D','‘D’ ward Office Building, Jobanputra Compound, Nana Chowk, Mumbai-400 007.'),
-	('Ward E','‘E’ ward Office Building, 10, Shaikh Haffizuddin Marg, Byculla, Mumbai-400 008.'),
-	('Ward F North','‘F/N’ ward Office Building, Plot, No.96, Bhau Daji Marg, Matunga, Mumbai-400 019.'),
-	('Ward F South','‘F/S’ ward Office Building, Jagganath Bhatankar Marg, &amp; Dr.B.A.Road Junction, Parel Naka, Mumbai-400 012.'),
-	('Ward G North','‘G/N’ ward Office Building, Harischandra Yewale Marg, Behind Plaza Cinema, Dadar, Mumbai-400 028.'),
-	('Ward G South','‘G/S’ ward Office Building, N.M.Joshi Marg, Elphinstone, Mumbai-400 018.'),
-	('Ward H East','H/E ward Office Building, Plot No.137 TPS-5 Prabhat Colony, Santacruz (E), Mumbai-400 051.'),
-	('Ward H West','‘H/W’ ward office Building, Saint Martin Road, Behind, Bandra Police Station , Bandra (West), Mumbai-400050.'),
-	('Ward K East','K/E, ward Office Building, Azad Road, Gundavali, Andheri(East), Mumbai-400 069.'),
-	('Ward K West','K/West ward Office Building, Paliram Road, Near, S.V. Road, Opp. Andheri Railway Station, Andheri(W), Mumbai-400 058.'),
-	('Ward L','Municipal Market Building, S.G.Barve Road, Kurla (West) , Mumbai – 400070.'),
-	('Ward M East','M/E Ward Office Bldg., Building Flat no.38 &amp; 39, Village Devnar, M.T. Kadam Marg, Peri Feri Road, Devnar Landmark Devnar Colony, Mumabi-400 043'),
-	('Ward M/ West','M/W ward Office Building, Sharadbhau Acharya Marg, Near Natraj Cinema, Chember, Mumbai-400 071.'),
-	('Ward N','N ward Annex Building, Jawahar Road, Ghatkopar (E), Mumbai-400 077.'),
-	('Ward P North','P/N Ward Office Building, Liberty Garden, Mamletdarwadi Marg, Malad (West), Mumbai-400 064.'),
-	('Ward P South','P/S Ward Office Building, Opp.City Center Shopping Mall, S.V. Road, Goregaon, Mumbai-400 064.'),
-	('Ward R Central','R/C Ward Office Building, Palika Mandai Buiding, S.V. Raod, Near Boriwali, Railway Station, Borivali (West), Mumbai – 400092.'),
-	('Ward R North','R/N Ward Office Building, Sangeetkar Sudhir Phadkey Flyover Bridge, Jaywant Sawant Marg, Dahisar (West), Mumbai-400 068.'),
-	('Ward R South','R/South Ward Office Building, Mahatma Gandhi Cross Road No.2, Near S.V.P. Swimming Pool, Kandivali (West), Mumbai – 400 067.'),
-	('Ward S','S Ward Office Building, Lalbahadur Shashtri Marg, Near Mangatram Petrol Pump, Bhandup (West), Mumbai – 400078.'),
-	('Ward T','T Ward Office Building, Lala Devidayal Marg, Mulund (W), Mumbai – 400080.');
+	('Ward A','‘A’ Ward Office Building, 134 ‘E’ Shahid Bhagat Singh Marg, Near R.B.I., Fort, Mumbai-400001.','Fort',400001),
+	('Ward B','‘B’ Ward Office Building, 121, Ramchandra Bhatt Marg , Opp.J.J.Hospital , Mumbai-400 009.','Opp.J.J.Hospital',400009),
+	('Ward C','‘C’ ward Office Building, 76, Shrikant Palekar Marg, Sonapur Street, Chira Bazar-Kalbadevi, Mumbai – 400002, Near Marine Lines Railway Station, Chandanwadi.','Chira Bazar-Kalbadevi',400002),
+	('Ward D','‘D’ ward Office Building, Jobanputra Compound, Nana Chowk, Mumbai-400 007.','Nana Chowk',400007),
+	('Ward E','‘E’ ward Office Building, 10, Shaikh Haffizuddin Marg, Byculla, Mumbai-400 008.','Byculla',400008),
+	('Ward F North','‘F/N’ ward Office Building, Plot, No.96, Bhau Daji Marg, Matunga, Mumbai-400 019.','Matunga',400019),
+	('Ward F South','‘F/S’ ward Office Building, Jagganath Bhatankar Marg, &amp; Dr.B.A.Road Junction, Parel Naka, Mumbai-400 012.','Parel Naka',400012),
+	('Ward G North','‘G/N’ ward Office Building, Harischandra Yewale Marg, Behind Plaza Cinema, Dadar, Mumbai-400 028.','Dadar',400028),
+	('Ward G South','‘G/S’ ward Office Building, N.M.Joshi Marg, Elphinstone, Mumbai-400 018.','Elphinstone',400018),
+	('Ward H East','H/E ward Office Building, Plot No.137 TPS-5 Prabhat Colony, Santacruz (E), Mumbai-400 051.','Santacruz (E)',400051),
+	('Ward H West','‘H/W’ ward office Building, Saint Martin Road, Behind, Bandra Police Station , Bandra (West), Mumbai-400050.','Bandra (West)',400050),
+	('Ward K East','K/E, ward Office Building, Azad Road, Gundavali, Andheri(East), Mumbai-400 069.','Andheri(East)',400069),
+	('Ward K West','K/West ward Office Building, Paliram Road, Near, S.V. Road, Opp. Andheri Railway Station, Andheri(W), Mumbai-400 058.','Andheri(W)',400058),
+	('Ward L','Municipal Market Building, S.G.Barve Road, Kurla (West) , Mumbai – 400070.','Kurla (West)',400070),
+	('Ward M East','M/E Ward Office Bldg., Building Flat no.38 &amp; 39, Village Devnar, M.T. Kadam Marg, Peri Feri Road, Devnar Landmark Devnar Colony, Mumabi-400 043','Devnar Landmark Devnar Colony',400043),
+	('Ward M/ West','M/W ward Office Building, Sharadbhau Acharya Marg, Near Natraj Cinema, Chember, Mumbai-400 071.','Chember',400071),
+	('Ward N','N ward Annex Building, Jawahar Road, Ghatkopar (E), Mumbai-400 077.','Ghatkopar (E)',400077),
+	('Ward P North','P/N Ward Office Building, Liberty Garden, Mamletdarwadi Marg, Malad (West), Mumbai-400 064.','Malad (West)',400064),
+	('Ward P South','P/S Ward Office Building, Opp.City Center Shopping Mall, S.V. Road, Goregaon, Mumbai-400 064.','Goregaon',400064),
+	('Ward R Central','R/C Ward Office Building, Palika Mandai Buiding, S.V. Raod, Near Boriwali, Railway Station, Borivali (West), Mumbai – 400092.','Borivali (West)',400092),
+	('Ward R North','R/N Ward Office Building, Sangeetkar Sudhir Phadkey Flyover Bridge, Jaywant Sawant Marg, Dahisar (West), Mumbai-400 068.','Dahisar (West)',400068),
+	('Ward R South','R/South Ward Office Building, Mahatma Gandhi Cross Road No.2, Near S.V.P. Swimming Pool, Kandivali (West), Mumbai – 400 067.','Kandivali (West)',400067),
+	('Ward S','S Ward Office Building, Lalbahadur Shashtri Marg, Near Mangatram Petrol Pump, Bhandup (West), Mumbai – 400078.','Bhandup (West)',400078),
+	('Ward T','T Ward Office Building, Lala Devidayal Marg, Mulund (W), Mumbai – 400080.','Mulund (W)',400080);
 
 /*!40000 ALTER TABLE `ward` ENABLE KEYS */;
 UNLOCK TABLES;
