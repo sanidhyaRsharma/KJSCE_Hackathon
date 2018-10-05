7c68e0a3589c4e3a1635a2c736446991e03216b5
Ameya Daddikar <ameyadaddikar@gmail.com>
2018-10-05 18:04:55 +0530

    Added upvote count to get_cards and added wards list text file to git

--- app/restful_routes.py
+++ app/restful_routes.py
@@ -4,6 +4,10 @@ import json
 
 @app.route('/get_cards', methods=['GET','POST'])
 def get_cards():
-    CURSOR.execute('SELECT * FROM card')
+    get_cards_query = "SELECT card.card_id AS card_id, timestamp, title, lat, lng, description, image, category, ward, \
+                        COUNT(upvote.card_id) AS upvotes FROM card \
+                        LEFT JOIN upvote ON card.card_id = upvote.card_id \
+                        GROUP BY card.card_id"
+    CURSOR.execute(get_cards_query)
     result = CURSOR.fetchone()
     return json.dumps(result, indent=4, sort_keys=True, default=str)
